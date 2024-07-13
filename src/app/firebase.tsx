afe92ef6500b6a28197d87028b55d869cc9e5625
Nwosu Chidinma <chidinmaifynwosu@gmail.com>
2024-07-13 19:20:15 +0100

    Scaffolded a project next.js and added firebase

---
+++ src/app/firebase.tsx
@@ -0,0 +1,18 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAy1cR0wd3E6WXYx0UmQ58hMXnPP4DG9s0",
+  authDomain: "chidinma-carefinder.firebaseapp.com",
+  projectId: "chidinma-carefinder",
+  storageBucket: "chidinma-carefinder.appspot.com",
+  messagingSenderId: "942436299437",
+  appId: "1:942436299437:web:880ab288c38e44188d0c24"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
